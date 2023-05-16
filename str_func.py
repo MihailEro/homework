a5705813be50ceab3300eb9a96ed3c03a9643db8
MihailEro <mihailerohin235@gmail.com>
2023-05-17 01:01:32 +0300

    add function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def get_upper(line):
+    """Делает все буквы заглавными"""
     return line.upper()
+
+def get_title(line):
+    """Делает первые буквы слова заглавными"""
+    return line.title()
+
